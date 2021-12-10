27cba8cc4805eb2758ab38b931adc5665652d6b6
q2lamko <Avenger_adamant@mail.ru>
2021-12-10 13:39:06 +0700

    reactmemo+useMemo

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,10 +1,8 @@
-import React, {useMemo, useState} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 
 export default {
     title: "useMemo select"
 }
-
-
 export const DifficultCounting = () => {
     const [a, setA] = useState(5)
     const [b, setB] = useState(5)
@@ -47,25 +45,21 @@ export const DifficultCounting = () => {
 }
 
 
-
 const UsersSecret = (props: { users: Array<string> }) => {
     console.log("usersecret render")
     return <div>{
         props.users.map((u, i) => <div key={i}>{u}</div>)
     }</div>
 }
-
 const Users = React.memo(UsersSecret)
-
-
 export const HelpsToReactMemo = () => {
     console.log('render')
 
     const [counter, setCounter] = useState(0)
-    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem','Armatur'])
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Armatur'])
 
     const filteredUsers = useMemo(() => {
-        return users.filter(u => u.indexOf('a' ) > -1)
+        return users.filter(u => u.indexOf('a') > -1)
     }, [users])
 
     const addUser = () => {
@@ -81,3 +75,54 @@ export const HelpsToReactMemo = () => {
         <Users users={filteredUsers}/>
     </>
 }
+
+const BooksMemo = (props: { books: Array<string>, addBook: () => void }) => {
+    console.log("books render")
+    return (
+        <div>
+            <button onClick={() => props.addBook()}>add book</button>
+            {
+                props.books.map((book, i) =>
+                    <div key={i}>
+                        {book}
+                    </div>)
+            }
+        </div>
+    )
+}
+
+const Books = React.memo(BooksMemo)
+
+export const useMemoLikeUseCallback = () => {
+    console.log('like useCallback')
+    const [counter, setCounter] = useState(0)
+    const [books, setBooks] = useState(['Flanagan', "Kit grant", "Frisby M.", "Fauler M."])
+
+    const filteredBooks = useMemo(() => {
+        return books.filter(book => book.indexOf('a') > -1)
+    }, [books])
+
+    const addBook = () => {
+        let NewBooks = [...books, 'TypeScript Udemya'];
+        setBooks(NewBooks)
+    }
+
+    const addMemoizedBook = useMemo(() => {
+        return addBook
+    }, [books])
+    // или используем useCallback
+    const memoisedBook = useCallback(() => {
+        let NewBooks = [...books, 'TypeScript Udemya'];
+        setBooks(NewBooks)
+    }, [books])
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
+        <Books
+            books={filteredBooks}
+            addBook={memoisedBook}
+        />
+    </>
+}
+
